e290a1f2bc25a2ddc4e41f7c8332cc658fa1d3ab
Manuel Schnabel <mschnabel1992@gmail.com>
2023-01-02 14:09:59 +0100

    [RELEASE] 1.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
-$EM_CONF['webm'] = [
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Creates WebM copies of videos',
     'description' => 'Creates WebM copies of videos',
     'category' => 'fe',
     'author' => 'Manuel Schnabel',
     'author_email' => 'service@passionweb.de',
-    'state' => 'stable',
     'author_company' => 'PassionWeb Manuel Schnabel',
+    'state' => 'stable',
     'uploadfolder' => false,
     'createDirs' => '',
     'clearCacheOnLoad' => true,
