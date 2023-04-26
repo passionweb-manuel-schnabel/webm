8deac43b01205a62fc8e189115d633fd10c228d4
Manuel Schnabel <mschnabel1992@gmail.com>
2023-04-26 14:16:14 +0200

    [TASK] Added compatibility with TYPO3 v12

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'clearCacheOnLoad' => true,
     'version' => '2.0.0',
     'constraints' => [
-        'depends' => ['typo3' => '12.0.0-12.2.0'],
+        'depends' => ['typo3' => '12.0.0-12.4.99'],
         'conflicts' => [],
         'suggests' => [],
     ],
