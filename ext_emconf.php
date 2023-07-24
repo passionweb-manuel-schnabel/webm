5618c8a28eec7a7fbed96f1ebded3eed1fdf3c60
Manuel Schnabel <mschnabel1992@gmail.com>
2023-07-24 23:20:18 +0200

    [RELEASE] Update version to 2.1.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'PassionWeb Manuel Schnabel',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'version' => '2.1.0',
+    'version' => '2.1.1',
     'constraints' => [
         'depends' => ['typo3' => '12.0.0-12.4.99'],
         'conflicts' => [],
