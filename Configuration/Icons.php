eef5543e29f34c8ac9794b12714a01c94acbc23c
Manuel Schnabel <service@passionweb.de>
2025-01-06 08:49:27 +0100

    [TASK] change logo from png to svg

---
+++ Configuration/Icons.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    'tx-passionweb-webm' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:webm/Resources/Public/Icons/Extension.svg',
+    ]
+];
